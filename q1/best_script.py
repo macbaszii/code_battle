5145034907257dc2c2b304a68bcc0105214b904c
Kiattisak Anoochitarom <macbaszii@gmail.com>
2013-08-04 23:49:43 +0700

    edit write to file logic

--- q1/best_script.py
+++ q1/best_script.py
@@ -2,6 +2,7 @@ numbers_file = open("numbers.txt");
 numbers_list = sorted([int(x) for x in (numbers_file.readlines())])
 
 output_file = open("run_result.txt", "w")
+missing_numbers = []
 
 for i in range(len(numbers_list)):
 	if i != (len(numbers_list) - 1):
@@ -9,14 +10,15 @@ for i in range(len(numbers_list)):
 			for j in range(1, (numbers_list[i + 1] - numbers_list[i])):
 				miss_number = numbers_list[i] + j;
 				if 0 <= miss_number <= 9:
-					output_file.write('000%d\n' % (miss_number))
+				    missing_numbers.append('000%d\n' % (miss_number))
 				elif 10 <= miss_number <= 99:
-					output_file.write('00%d\n' % (miss_number))
+				    missing_numbers.append('00%d\n' % (miss_number))
 				elif 100 <= miss_number <= 999:
-					output_file.write('0%d\n' % (miss_number))
+				    missing_numbers.append('0%d\n' % (miss_number))
 				else:
-					output_file.write('%d\n' % (miss_number))
-			
+				    missing_numbers.append('%d\n' % (miss_number))
+
+output_file.writelines(missing_numbers)
 numbers_file.close()
 output_file.close()
 			
