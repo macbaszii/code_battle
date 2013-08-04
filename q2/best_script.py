6cc99fe8fe7c06faf4ee9be17b4ccdc9c5d7fc1d
Kiattisak Anoochitarom <macbaszii@gmail.com>
2013-08-04 23:59:19 +0700

    forget strip \n from input and edit write to file logic in q2

--- q2/best_script.py
+++ q2/best_script.py
@@ -1,9 +1,15 @@
 from collections import Counter
 
 numbers_file = open("numbers.txt")
-numbers_list = numbers_file.readlines()#[x for x in ()]
+numbers_list = [x.strip() for x in numbers_file.readlines()]
 
 output_file = open("run_result.txt", "w")
 
+result_list = []
+
 for (a, b) in [(x, y) for (x, y) in Counter(numbers_list).most_common(len(set(numbers_list))) if y != 1]:
-	output_file.write('\"%s\", %d\n' % (str(a), b))
+	result_list.append('"%s", %d\n' % (str(a), b))
+
+output_file.writelines(result_list)
+output_file.close()
+numbers_file.close()
